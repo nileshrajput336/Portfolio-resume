5ff5421e47ba87af3989d7495bb8179ed7aa7f39
Nilesh <nileshrajput7648@gmail.com>
2024-11-19 11:57:12 +0530

    Initial commit

---
+++ Balram Personal Portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ======================== */
+var typed = new Typed(".typing", {
+    strings: ["","Php Developer", "Web Developer", "Web Designer", "Backend Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
